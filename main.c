f181b557c91b013faf0c5933258ca5511f05bab7
Tirziu Dragos <56274360+dragos3100@users.noreply.github.com>
2019-10-18 13:37:32 +0300

    ex

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    printf("Let's chat\n");
+    char userImput[20];
+    gets(userImput);
+    printf("Hi! to you too. Iam Hall What's your name?\n");
+    char name[20];
+    scanf("%s",name);
+    printf("Oh hi, %s What's your age?\n",name);
+    int age;
+    scanf("%d",&age);
+    printf("Wow. %d is quite something\n",age);
+    gets(userImput);
+    gets(userImput);
+    printf("Sory, not sure what this\"%s\" means Bye",userImput);
+
+
     return 0;
 }
